e67ea451f6f57deb5811e2b764383dae85578de7
VDima228231 <dimkaavito228@gmail.com>
2023-12-15 13:55:01 +0300

    Добавьте файлы проекта.

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>;
+#include <cmath>;
+using namespace std;
+int main(){
+	setlocale(LC_ALL, "Russian");
+	cout << "Калькулятор Димона" << endl;
+
+	double oper1, oper2;
+	char operation;
+
+	cout << "Введите выражение: ";
+	cin >> oper1 >> operation >> oper2;
+
+	double result;
+
+	switch (operation) {
+	case '+':
+		result = oper1 + oper2;
+		break;
+	case '-':
+		result = oper1 - oper2;
+		break;
+	case '*':
+		result = oper1 * oper2;
+		break;
+	case '/':
+		result = oper1 / oper2;
+		break;
+	}
+
+	cout << "Результат: " << result;
+	return 0;
+}
